b89212f18c5e41326651b8ba31aedd607bb2a42e
EzeOkK <135455528+EzeOkK@users.noreply.github.com>
2023-10-09 10:34:27 -0300

    WALL-spam.py

---
+++ Scripts/WALL-spam.py
@@ -0,0 +1,38 @@
+import aminofix as amino
+import time
+
+print(
+    f"""
+ __      __     ______      __         __        
+/\ \  __/\ \   /\  _  \    /\ \       /\ \       
+\ \ \/\ \ \ \  \ \ \L\ \   \ \ \      \ \ \      
+ \ \ \ \ \ \ \  \ \  __ \   \ \ \  __  \ \ \  __ 
+  \ \ \_/ \_\ \  \ \ \/\ \   \ \ \L\ \  \ \ \L\ \
+   \ `\___x___/   \ \_\ \_\   \ \____/   \ \____/
+    '\/__//__/     \/_/\/_/    \/___/     \/___/ 
+                                                 
+                                                     
+
+ ⠀ ⠀ ⠀ ⠀ ⠀ ⠀ ⠀ ⠀spam !! 🇧🇷/🇺🇲
+""")
+
+client = amino.Client()
+
+email = "j7primehfif@gmail.com"
+password = "Botamino123"
+client.login(email=email, password=password)
+
+link_info = client.get_from_code(input("-- User link::: "))
+com_id = link_info.comId
+user_id = link_info.objectId
+
+sub_client = amino.SubClient(comId=com_id, profile=client.profile)
+message = "--"
+
+while True:
+    try:
+        sub_client.comment(message, userId=user_id)
+        print("-- Comment is sent...")
+        time.sleep(7) # 7 s to avoid forBidden and punishments from communities.
+    except Exception as e:
+        print(e)
